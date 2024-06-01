d47d2a65a098467ea7575b2af6cc078d12479ce0
Samiun Alim Auntor <samiunalimauntor@gamil.com>
2024-06-01 17:09:52 +0600

    Completed Splash class

--- src/Splash.java
+++ src/Splash.java
@@ -1,2 +1,36 @@
-public class Splash {
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+
+    Splash() {
+
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
+        heading.setBounds(75, 10,650,50);
+        heading.setFont(new Font("serif", Font.BOLD, 30));
+        heading.setForeground(Color.BLACK);
+        add(heading);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/Splash.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(700, 700, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 690, 665);
+        add(image);
+
+        JButton clickHere = new JButton("CLICK HERE TO CONTINUE");
+        clickHere.setBounds(100, 600, 500, 50);
+        image.add(clickHere);
+
+        setSize(700,700);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
 }
